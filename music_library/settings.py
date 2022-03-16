2a38ad82fe04f57157525fb1c498fedd1c6cc0b4
Zaie Hilmi <zaiehilmi@gmail.com>
2022-03-16 08:52:18 +0800

    hide cred

--- music_library/settings.py
+++ music_library/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8eig0o&5ei&9_fzmrbnllude565ax6n#d#&^x2m^(=2-hlcde*'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
